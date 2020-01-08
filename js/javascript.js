019ace8703a2d639881d30531e5e77dea74f3260
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-08 16:21:11 +0100

    wip

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi,
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
+var listaCognomi = ['Rossi', 'Di Pasquale', 'Ventura', 'Verdi', 'Casale']
+console.log(listaCognomi);
+
+var cognome = prompt('Inserisci il tuo cognome');
+while(cognome.length <= 0){
+  cognome = prompt('Inserisci il tuo cognome');
+}
+console.log(cognome);
+listaCognomi.push(cognome);
+console.log(listaCognomi);
+
+for(var i = 0; i < listaCognomi.length; i++){
+  listaCognomi.sort;
+  document.getElementById("listaOrdinata").innerHTML += '<li>' + listaCognomi[i] + '</li>';
+}
